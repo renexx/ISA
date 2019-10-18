5744fe0b9b4cb92bbc89eb1e606f6240435693d3
renexx <rbolf.rene@gmail.com>
2019-10-18 17:41:41 +0200

    ziadny progress

--- isa-tazatel.cpp
+++ isa-tazatel.cpp
@@ -28,6 +28,8 @@ using namespace std;    // Or using std::string;
 //#include <getopt.h> // for getopt_long
 #define BUFFER 65535
 #define N 4096
+#define lanswer 4096
+#define ldname  65535
 void print_usage()
 {
     printf("-q<IP|hostname>, povinny argument\n");
@@ -42,12 +44,12 @@ void PrintRegexMatch(std::string str, std::regex reg)
     //std::cout << std::boolalpha;
     while(std::regex_search(str,match,reg))
     {
-
         std::cout << match.str() << "\n";
         str = match.suffix().str();
-
     }
 }
+
+
 
 int main(int argc, char **argv) {
     int option;
@@ -63,16 +65,18 @@ int main(int argc, char **argv) {
     bool w_flag = false;
     bool d_flag = false;
     char hostname[100], whois[100], dns[100];
+
     char buf[BUFFER];
     int msg_size;
     int i = 0;
-    u_char nsbuf[N];
-    char dispbuf[N];
+    u_char pResAnswer[lanswer], Uncompressed[ldname];
+    //char dispbuf[N];
     ns_msg msg;
     ns_rr rr;
-    int x, l;
-    struct addrinfo hints, *infoptr;
-    int result;
+    const u_char *p;
+    int x, l,nResAnswerLen;
+    struct addrinfo whois_server, *whois_infoptr, *whois_ptr, client_adress, *client_infoptr, *client_ptr;
+    int result_for_whois, result_for_client;
 
     string input;
     std::regex inetnumReg("inetnum:.*");
@@ -128,138 +132,93 @@ int main(int argc, char **argv) {
                    print_usage();
        }
    }
+   // HEADER
+         cout << "======== DNS:\t" << dns << "=============\n";
+         if(nResAnswerLen = (res_search(hostname,ns_c_in,ns_s_an,pResAnswer,lanswer)) < 0)
+         {
+             fprintf(stderr, "ERROR tu\n");
+             exit(EXIT_FAILURE);
+         }
+         if(ns_initparse(pResAnswer,l,&msg))
+         {
+             fprintf(stderr, "ERROR nie tu\n");
+             exit(EXIT_FAILURE);
+         }
+         for(x = 0; x < ns_msg_count(msg,ns_s_an); x++)
+         {
+             if(ns_parserr(&msg,ns_s_an,x,&rr) < 0)
+             {
+                 fprintf(stderr, "ERROR\n");
+                 exit(EXIT_FAILURE);
+             }
+             switch(ns_rr_type(rr))
+             {
+                 case ns_t_a:
+                    cout << "A" << ns_rr_name(rr);
+                    p = ns_rr_rdata(rr);
+             }
+         }
+
+
 
      /* 2. ziskani adresy serveru pomoci DNS  ziska IP adresu z domeny dotazuje sa na DNS zaznam A*/
 
+     memset(&whois_server,0,sizeof(whois_server));  //nastavy dany pocet bytov na hodnotu uvedenu v parametri c
+     whois_server.ai_family = AF_INET;
+     whois_server.ai_socktype = SOCK_STREAM;
+     whois_server.ai_protocol = IPPROTO_TCP;
 
-
-
-     memset(&hints,0,sizeof(hints));  //nastavy dany pocet bytov na hodnotu uvedenu v parametri c
-     hints.ai_family = AF_UNSPEC;
-     hints.ai_socktype = SOCK_STREAM;
-     hints.ai_protocol = IPPROTO_TCP;
-
-     result = getaddrinfo(whois,"43",&hints,&infoptr);
-     if(result != 0)
+     result_for_whois = getaddrinfo(whois,"43",&whois_server,&whois_infoptr);
+     if(result_for_whois != 0)
      {
-         fprintf(stderr, "%s: %s\n", whois, gai_strerror(result));
+         fprintf(stderr, "%s: %s\n", whois, gai_strerror(result_for_whois));
          exit(EXIT_FAILURE);
      }
      cout <<"WHOIS DOMEN NAME:\t " << whois << "\n";
-     struct addrinfo *p;
-     for(p = infoptr;p != NULL; p = p->ai_next)
+
+     for(whois_ptr = whois_infoptr; whois_ptr != NULL; whois_ptr = whois_ptr->ai_next)
      {
-         getnameinfo(p->ai_addr,p->ai_addrlen,whois,sizeof(whois),NULL,0,NI_NUMERICHOST);
-         cout << "WHOIS IP ADRESS:\t" << whois << "\n";
+         getnameinfo(whois_ptr->ai_addr,whois_ptr->ai_addrlen,whois,sizeof(whois),NULL,0,NI_NUMERICHOST);
+
+         /* Vytvoreni soketu a inicializovanie soketu*/
+         if ((client_socket = socket(whois_infoptr->ai_family, whois_infoptr->ai_socktype, whois_infoptr->ai_protocol)) <= 0) /*AF_INET = IPv4, SOCK_STREAM = TCP, 0 je protokol 0 implicitne vybere podla SOCK_STREAM, inak IPPROTO_TCP*/
+         {
+             perror("ERROR: socket");
+             exit(EXIT_FAILURE);
+         }
+
+         /*Aktivne otvorenie na strane klienta, druhy parameter funkcie obsahuje ip adresu a port servera*/
+         if (connect(client_socket, whois_ptr->ai_addr, whois_ptr->ai_addrlen) != 0)
+         {
+             perror("ERROR: connect");
+             exit(EXIT_FAILURE);
+         }
      }
-     freeaddrinfo(infoptr);
-     return 0;
+     cout <<"WHOIS IP ADDRESS:\t " << whois << "\n";
 
+     memset(&client_adress,0,sizeof(client_adress));
+     client_adress.ai_family = AF_INET;
+     client_adress.ai_socktype = SOCK_STREAM;
+     client_adress.ai_protocol = IPPROTO_TCP;
 
-     memcpy(&klient_adress.sin_addr,he->h_addr,he->h_length); // porovnava retazec bytov , kopiruje specifikovany pocet bytov do cielovej struktury
-
-    /* // HEADER
-        cout << "======== DNS:\t" << dns << "=============\n";
- // -----
-        l = res_query(hostname,ns_c_any,ns_t_aaaa,nsbuf,sizeof(nsbuf));
-        if(l < 0)
-        {
-            perror(hostname);
-        }
-        ns_initparse(nsbuf,l,&msg);
-        //l= ns_msg_count(msg,ns_s_an);
-        ns_parserr(&msg, ns_s_an, i, &rr);
-        ns_sprintrr(&msg, &rr, NULL, NULL, dispbuf, sizeof(dispbuf));
-        printf("%s \n", dispbuf);
-
-
-        l = res_query(hostname,ns_c_any,ns_t_a,nsbuf,sizeof(nsbuf));
-        if(l < 0)
-        {
-            perror(hostname);
-        }
-        ns_initparse(nsbuf,l,&msg);
-        //l= ns_msg_count(msg,ns_s_an);
-        ns_parserr(&msg, ns_s_an, i, &rr);
-        ns_sprintrr(&msg, &rr, NULL, NULL, dispbuf, sizeof(dispbuf));
-        printf("\n%s \n", dispbuf);
-
-        l = res_query(hostname,ns_c_any,ns_t_mx,nsbuf,sizeof(nsbuf));
-        if(l < 0)
-        {
-            perror(hostname);
-        }
-        ns_initparse(nsbuf,l,&msg);
-        //l= ns_msg_count(msg,ns_s_an);
-        ns_parserr(&msg, ns_s_an, i, &rr);
-        ns_sprintrr(&msg, &rr, NULL, NULL, dispbuf, sizeof(dispbuf));
-        printf("%s \n", dispbuf);
-
-        l = res_query(hostname,ns_c_any,ns_t_cname,nsbuf,sizeof(nsbuf));
-        if(l < 0)
-        {
-            perror(hostname);
-        }
-        ns_initparse(nsbuf,l,&msg);
-        //l= ns_msg_count(msg,ns_s_an);
-        //ns_parserr(&msg, ns_s_an, i, &rr);
-        //ns_sprintrr(&msg, &rr, NULL, NULL, dispbuf, sizeof(dispbuf));
-        //rintf("toto je cname%s, a toto je name %s \n", dispbuf, ns_rr_name());
-
-        /*printf("NS:");
-           l = res_query(hostname, ns_c_any, ns_t_ns, nsbuf, sizeof(nsbuf));
-           if (l < 0)
-           {
-             perror(hostname);
-           }
-           ns_initparse(nsbuf, l, &msg);
-           l = ns_msg_count(msg, ns_s_an);
-
-           ns_parserr(&msg, ns_s_an, 0, &rr);
-           ns_sprintrr(&msg, &rr, NULL, NULL, dispbuf, sizeof(dispbuf));
-           printf("%s \n", dispbuf);
-*/
-
-
-
-
-    // printf("A: \t%s \n", inet_ntoa(klient_adress.sin_addr));
-
-     //printf("CNAME: %s\n",he->h_name);
-
-     char *daco = (char*)malloc(sizeof(char) * 1024);
-     strcpy(daco,inet_ntoa(klient_adress.sin_addr));
-
-     memset(&ipv6,0,sizeof(ipv6));  //nastavy dany pocet bytov na hodnotu uvedenu v parametri c
-     ipv6.sin6_family = AF_INET6; /* IPV4*/
-     memcpy(&ipv6.sin6_addr,he->h_addr,he->h_length); // porovnava retazec bytov , kopiruje specifikovany pocet bytov do cielovej struktury
-//     inet_pton(AF_INET6, hostname, &(ipv6.sin6_addr));
-
-     char str[INET6_ADDRSTRLEN];
-
-    // str = hostname;
-     inet_ntop(AF_INET6,&ipv6.sin6_addr,str,sizeof(str));
-    // cout << "AAAA:\t" <<str << "\n";
-
-
-    /* Vytvoreni soketu a inicializovanie soketu*/
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) <= 0) /*AF_INET = IPv4, SOCK_STREAM = TCP, 0 je protokol 0 implicitne vybere podla SOCK_STREAM, inak IPPROTO_TCP*/
-    {
-        perror("ERROR: socket");
-        exit(EXIT_FAILURE);
-    }
-
-    /*Aktivne otvorenie na strane klienta, druhy parameter funkcie obsahuje ip adresu a port servera*/
-    if (connect(client_socket, (const struct sockaddr *) &server_address, sizeof(server_address)) != 0)
-    {
-        perror("ERROR: connect");
-        exit(EXIT_FAILURE);
-    }
+     result_for_client = getaddrinfo(hostname,NULL,&client_adress,&client_infoptr);
+     if(result_for_client != 0)
+     {
+         fprintf(stderr, "%s: %s\n", hostname, gai_strerror(result_for_client));
+         exit(EXIT_FAILURE);
+     }
+     cout <<"CLIENT DOMEN NAME:\t " << hostname << "\n";
+     for(client_ptr = client_infoptr; client_ptr != NULL; client_ptr = client_ptr->ai_next)
+     {
+         getnameinfo(client_ptr->ai_addr,client_ptr->ai_addrlen,hostname,sizeof(hostname),NULL,0,NI_NUMERICHOST);
+         cout <<"CLIENT IP ADDRESS:\t " << hostname << "\n";
+     }
 
     /* odeslani zpravy na server */
 
-    strcat(daco,"\n");
-    strcpy(buf,daco);
+    strcat(hostname,"\n");
+    cout << hostname;
+    strcpy(buf,hostname);
     while(i < 2)
      {
         /* code */
@@ -279,19 +238,20 @@ int main(int argc, char **argv) {
     }
 
     //    printf("%.*s\n",byteread,buf);
-        input = buf;
-
-        cout << "====== WHOIS ===========\n";
-        PrintRegexMatch(input,inetnumReg);
-        PrintRegexMatch(input,netnameReg);
-        PrintRegexMatch(input,descrReg);
-        PrintRegexMatch(input,countryReg);
-        PrintRegexMatch(input,admin_cReg);
-        PrintRegexMatch(input,addressReg);
-        PrintRegexMatch(input,phoneReg);
+    input = buf;
+    cout << "====== WHOIS ===========\n";
+    PrintRegexMatch(input,inetnumReg);
+    PrintRegexMatch(input,netnameReg);
+    PrintRegexMatch(input,descrReg);
+    PrintRegexMatch(input,countryReg);
+    PrintRegexMatch(input,admin_cReg);
+    PrintRegexMatch(input,addressReg);
+    PrintRegexMatch(input,phoneReg);
 
    close(client_socket);
    printf("\n\n* Closing client socket ...\n");
-   free(daco);
-  return 0;
+   //free(daco);
+   freeaddrinfo(whois_infoptr);
+   freeaddrinfo(client_infoptr);
+   return 0;
 }
